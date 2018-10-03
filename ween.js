3dad8c3f166f469b3441f82479a2417a48da39af
Emma Highland <ehighland@luc.edu>
2018-10-03 14:04:43 -0600

    Implemented shuffle function with list added content to all pages and cleaned up styling

--- ween.js
+++ ween.js
@@ -1,11 +1,11 @@
-var checklist = "<br />blah blah testing"
+var movies = ["Mr. Jones", "The Perfect Host", "Suburban Gothic", "Prom Night", "Revenge", "The Canal", "Psycho", "Wolfcop", "Sonno Profundo", "Take Shelter", "Lovely Molly", "A Tale of Two Sisters", "Shadow of Doubt", "Dead of Night", "Der Nachtmahr", "The Substitute", "Vampyros Lesbos", "Alena", "Let Us Prey", "Rabid", "The House That Dripped Blood", "Sleep Tight", "Cherry Tree", "The Wicker Man (1973)", "Southbound", "Blood and Black Lace", "And Soon the Darkness", "Are We Not Cats", "Bug","Coherence", "Day of the Dead"];
 
-document.getElementById("select").addEventListener("click", choice);
-document.getElementById("all").addEventListener("click",choice);
+document.getElementById("shuffle").addEventListener("click", shuffle);
 
-function choice() {
-	if (this.id === "select"){
-		document.getElementById("list").innerHTML = checklist;
-	}
+function shuffle() {
+	var x = Math.floor((Math.random() * movies.length) +1) -1;
+	var output = movies[x];
+
+	document.getElementById("output").innerHTML = output;
 }
 
